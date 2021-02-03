04898b23594adf8bd01adac349d18072c5295c35
Elisha Kramer <kramer1346@gmail.com>
2021-02-03 15:19:31 +0200

    country and category filters both working

--- api/controllers/news.js
+++ api/controllers/news.js
@@ -12,9 +12,14 @@ exports.getNews = async (req, res) => {
 exports.getCategoryNews = async (req, res) => {
   console.log('api/articles category called!');
 
-  console.log('req.params',req.params);  
-  // const headlines = await news.topic('BUSINESS', {n : 5});
-
   const headlines = await news.topic(req.params.category.toUpperCase(), {n : 5});
+  res.json(headlines);
+}
+
+
+exports.getCountryNews = async (req, res) => {
+  console.log('api/articles country called!');
+
+  const headlines = await news.geo(req.params.country.toUpperCase(), {n : 5});
   res.json(headlines);
 }
--- api/routes/news.js
+++ api/routes/news.js
@@ -1,31 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getNews, getCategoryNews } = require('../controllers/news')
-
+const { getNews, getCategoryNews, getCountryNews } = require('../controllers/news')
 
 router.get('/articles', getNews);
 router.get('/articles/category/:category', getCategoryNews);
-
+router.get('/articles/country/:country', getCountryNews);
 
 // router.post('/tag', create);
-// router.get('/tags', list);
-// router.delete('/tag/:slug', remove);
-// router.put('/tag/:slug', update);
-
-// place holder for the data
-const users = [];
-
-router.get('/users', (req, res) => {
-  console.log('api/users called!')
-  res.json(users);
-});
-
-router.post('/user', (req, res) => {
-  const user = req.body.user;
-  console.log('Adding user:::::', user);
-  users.push(user);
-  res.json("user addedd");
-});
 
 module.exports = router; 
